9a267a7bd8ab5fd91f0bea4b749061d2d5ec9215
Dorian <dorianemenir@gmail.com>
2022-03-01 14:32:43 +0100

    :construction: removed unnecessary space

--- components/FamousPlaces.js
+++ components/FamousPlaces.js
@@ -48,7 +48,6 @@ export default function FamousPlaces() {
                       objectFit="cover"
                     />
                   </div>
-
                   <span>{place.name}</span>
                 </a>
               </Link>
